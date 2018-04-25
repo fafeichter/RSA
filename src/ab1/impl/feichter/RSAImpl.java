2e24e060f5b0dfd433a08751b79b83312ed14d61
Fabian Feichter <mail@fabian-feichter.at>
2018-04-25 20:50:55 +0200

    Initial commit

---
+++ src/ab1/impl/feichter/RSAImpl.java
@@ -0,0 +1,48 @@
+package ab1.impl.feichter;
+
+import ab1.RSA;
+
+public class RSAImpl implements RSA {
+
+	@Override
+	public void init(int n) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public PublicKey getPublicKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PrivateKey getPrivateKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] encrypt(byte[] data, boolean activateOAEP) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] decrypt(byte[] data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] sign(byte[] message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean verify(byte[] message, byte[] signature) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
